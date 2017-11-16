f874f495cbe52d8ab5b8acacd6eabe277d163e72
MarcCristina <crys_20062006@yahoo.com>
2017-11-16 18:47:51 +0200

    project structure

---
+++ src/main/java/org/fasttrackit/Dog.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+public class Dog {
+    String name;
+    double age;
+    String healthCondition;
+    double weight;
+    String color;
+    String breed;
+    String role;
+
+
+
+}
---
+++ src/main/java/org/fasttrackit/DogFood.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+public class DogFood {
+    double price;
+    double quantity;
+    String type;
+    String brand;
+    String flavor;
+    String packaging;
+
+
+}
