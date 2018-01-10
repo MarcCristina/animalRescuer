6f2137b782eab5ea51df5cd115f5e93f59e0814e
MarcCristina <crys_20062006@yahoo.com>
2018-01-10 21:30:43 +0200

    working on ex.6 from homework nr.6

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,10 +1,57 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private Rescuer rescuer;
     private Dog dog;
     private DogFood dogFood;
     private Vet vet;
+    private List<Food> availableFood = new ArrayList<Food>();
+    private EntertaimentActivity[] availableActivities = new EntertaimentActivity[10];
+
+    private void initFood() {
+        Food food = new Food();
+        food.setFood("Akana");
+
+        Food food1 = new Food();
+        food.setFood("Royal");
+
+        for (int i = 0; i < availableFood(); i++) ;
+        {
+
+        }
+    }
+    private int availableFood() {
+        return 0;
+    }
+
+    private void initActivities(); {
+        EntertaimentActivity acttivities = new EntertaimentActivity();
+        acttivities.setName("Run");
+
+        EntertaimentActivity activity = new EntertaimentActivity();
+        activity.setName("hunting");
+
+
+    }
+
+    public List<Food> getAvaibleFood() {
+        return availableFood;
+    }
+
+    public void setAvaibleFood(List<Food> avaibleFood) {
+        this.availableFood = avaibleFood;
+    }
+
+    public EntertaimentActivity[] getAvaibleActivities() {
+        return availableActivities;
+    }
+
+    public void setAvaibleActivities(EntertaimentActivity[] avaibleActivities) {
+        this.availableActivities = avaibleActivities;
+    }
 
     public Rescuer getRescuer() {
         return rescuer;
